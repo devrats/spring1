5a88ff4fc5a9307ae98beba7c1cf674d1e8385f4
Sharma Ji <devvrat.sharm_cs19@gla.ac.in>
2021-04-29 20:41:45 +0530

    third commit.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        System.out.println("context.getBean(\"student1\") = " + context.getBean("student1"));
         SpringApplication.run(DemoApplication.class, args);
     }
 }
